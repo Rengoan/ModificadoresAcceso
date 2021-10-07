795bfe9ed41267b01e70e84aba1b7dd119ccf8aa
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-10-07 10:28:23 +0200

    modificadores

---
+++ Modifcadores/src/Test/Test.java
@@ -0,0 +1,12 @@
+package Test;
+
+import paquete1.Clase1;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Clase1 class1 = new Clase1();
+        System.out.println(class1.atributoPublico); //Se puede acceder al atributo publico de Clase1
+    }
+}
---
+++ Modifcadores/src/paquete3/Clase3.java
@@ -0,0 +1,7 @@
+package paquete3;
+
+import paquete1.Clase1;
+
+public class Clase3 extends Clase1 {
+
+}
